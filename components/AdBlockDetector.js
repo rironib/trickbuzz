6708de4feef3443c77f82654f05268cf6853978b
rironib <dev.ronib@gmail.com>
2025-08-24 02:37:54 +0600

    Update AdBlockDetector.js

--- components/AdBlockDetector.js
+++ components/AdBlockDetector.js
@@ -1,18 +1,10 @@
 "use client";
 
+import { useDisclosure } from "@heroui/react";
 import { useEffect } from "react";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from "@heroui/react";
 
 export default function AdBlockDetector() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen, onOpen } = useDisclosure();
 
   useEffect(() => {
     function getCookie(name) {
@@ -39,9 +31,8 @@ export default function AdBlockDetector() {
       const ck = getCookie("_dAB");
 
       if (ck == null) {
-        // Show modal instead of manipulating DOM manually
         setTimeout(() => {
-          onOpen(); // ✅ Open the modal
+          onOpen(); // <-- opens the banner
         }, 3000);
       }
     };
@@ -54,34 +45,20 @@ export default function AdBlockDetector() {
     e.parentNode.insertBefore(b, e);
   }, [onOpen]);
 
+  // Only show banner if adblock detected
+  if (!isOpen) return null;
+
   return (
-    <Modal isOpen={isOpen} placement="center"  backdrop="blur" onOpenChange={onOpenChange}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">
-              Ad Blocker Detected!!!
-            </ModalHeader>
-            <ModalBody>
-              <p>
-                Please disable your ad blocker to view this website properly.
-              </p>
-              <p>
-                To support us, please consider whitelisting our site in your ad
-                blocker.
-              </p>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={onClose}>
-                Ok
-              </Button>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
-    </Modal>
+    <div className="bg-warning-500 border-danger-500 fixed right-0 bottom-0 left-0 z-50 mx-auto flex min-h-20 w-full max-w-3xl items-center justify-center rounded-t-2xl border-t-2 text-center shadow-2xl">
+      <div className="space-y-2 p-2 pt-6 pb-4">
+        <div className="text-danger-500 text-xl font-bold">
+          Ad Blocker Detected!!!
+        </div>
+        <div className="text-sm">
+          To support us, please consider whitelisting our site in your ad
+          blocker.
+        </div>
+      </div>
+    </div>
   );
 }
